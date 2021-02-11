112c2f1c4ae2f3093973a6995715f493f88d6193
Sholanki <63105386+Sholanki@users.noreply.github.com>
2021-02-11 11:31:00 +0530

    Update notes.11tydata.js

--- notes/notes.11tydata.js
+++ notes/notes.11tydata.js
@@ -8,7 +8,7 @@ function caselessCompare(a, b) {
 }
 
 module.exports = {
-    layout: "note.html",
+    layout: "notesTempl.njk",
     type: "note",
     eleventyComputed: {
         title: data => titleCase(data.title || data.page.fileSlug),
